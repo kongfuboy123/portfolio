975e2ca32cc329628e078fe4f49560486586781b
kongfuboy123 <KONGFUBOYJW2@GMAIL.COM>
2022-07-05 14:17:40 +1000

    change ico to 4 star

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Product({img,link}) {
  
   return (
-    <div className='rounded-lg overflow-hidden drop-shadow-lg h-[30vh] md:h-[35vh] lg:h-[40vh]'>
+    <div className='rounded-lg overflow-hidden shadow-lg h-[30vh] md:h-[35vh] lg:h-[40vh]'>
       <div className='h-5 bg-gray-400 flex items-center sticky z-20'>
         <div className='h-[6px] w-[6px] rounded-full bg-white mr-[3px] ml-3'></div>
         <div className='h-[6px] w-[6px] rounded-full bg-white mr-[3px]'></div>
